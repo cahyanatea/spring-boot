852d55222944525ab529557428dad3cab13ecee0
cahyanatea003 <cahyanatea003@gmail.com>
2019-11-03 20:45:17 +0700

    001 Sederhana - spring-boot

---
+++ src/main/java/com/example/demo/HelloController.java
@@ -0,0 +1,25 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+	@RequestMapping(value = "/")
+	public String sayHello() {
+		return "Halo bro..";
+	}
+	
+	@PostMapping(value="/uang")
+	public String uang() {
+		return "hello";
+	}
+	
+	@GetMapping(value="/duit")
+	public int duit() {
+		return 1000;
+	}
+}
