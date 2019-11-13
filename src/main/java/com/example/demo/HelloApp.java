d94ac4282c0e83c24565422850695f83538037e5
Asep Cahyana <cahyanatea003@gmail.com>
2019-11-13 18:36:52 +0700

    Logging with log4j2

--- src/main/java/com/example/demo/HelloApp.java
+++ src/main/java/com/example/demo/HelloApp.java
@@ -1,5 +1,7 @@
 package com.example.demo;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,7 +9,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class HelloApp {
 
 	public static void main(String[] args) {
-		SpringApplication.run(HelloApp.class, args);
+            SpringApplication.run(HelloApp.class, args);
+                
+            Logger logger = LoggerFactory.getLogger(HelloApp.class);
+            logger.warn("PESAN WARN");
+            logger.error("PESAN ERROR");
+            logger.info("PESAN INFO");
+            logger.debug("PESAN DEBUG");
 	}
 
 }
