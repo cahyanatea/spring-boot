56fd747f5ac0febf5adb0a5893ec5e56839460b0
Asep Cahyana <cahyanatea003@gmail.com>
2019-11-15 17:32:13 +0700

    Mengganti pesan error pada /error

--- src/main/java/com/example/demo/controller/HelloController.java
+++ src/main/java/com/example/demo/controller/HelloController.java
@@ -1,12 +1,15 @@
 package com.example.demo.controller;
 
+import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class HelloController {
+public class HelloController implements ErrorController {
+    
+    private static final String ERROR_PATH = "/error";
 
 	@RequestMapping(value = "/")
 	public String sayHello() {
@@ -22,4 +25,14 @@ public class HelloController {
 	public int duit() {
 		return 1000;
 	}
+        
+    @GetMapping(value = ERROR_PATH)
+    public String pesanError() {
+        return "Page tidak ditemukan.";
+    }
+
+    @Override
+    public String getErrorPath() {
+        return ERROR_PATH;
+    }
 }
