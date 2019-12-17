7d89f8948eb8bd7824b0c30d97a26566d42e3743
Asep Cahyana <cahyanatea003@gmail.com>
2019-12-17 08:16:05 +0700

    Service untuk konfigurasi Profile

--- src/main/java/com/example/demo/HelloApp.java
+++ src/main/java/com/example/demo/HelloApp.java
@@ -1,12 +1,18 @@
 package com.example.demo;
 
+import com.example.demo.config.EnvBasedConfig;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class HelloApp {
+public class HelloApp implements CommandLineRunner {
+    
+    @Autowired
+    private EnvBasedConfig envBasedConfig;
 
 	public static void main(String[] args) {
             SpringApplication.run(HelloApp.class, args);
@@ -17,5 +23,11 @@ public class HelloApp {
             logger.info("PESAN INFO");
             logger.debug("PESAN DEBUG");
 	}
+
+    @Override
+    public void run(String... args) throws Exception {
+        System.out.println("---------------");
+        envBasedConfig.setup();
+    }
 
 }
---
+++ src/main/java/com/example/demo/config/DevProfileBean.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.example.demo.config;
+
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+/**
+ *
+ * @author okt19
+ */
+@Service
+@Profile("dev")
+public class DevProfileBean implements EnvBasedConfig {
+
+    @Override
+    public void setup() {
+        System.out.println("Menggunakan dev config");
+    }
+    
+}
